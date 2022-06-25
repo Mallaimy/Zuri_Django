a24b9498bbb382b9e1aa0b084108f213429dbf2a
Mallaimy <bokormallaimy@gmail.com>
2022-06-25 15:00:45 +0100

    django CRUD task

--- I4G005000HWO/I4G005000HWO/settings.py
+++ I4G005000HWO/I4G005000HWO/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')&p8tx9zw*v&(-=f#!764^xh3k31d#$m=-*#9-zypzb0s4_u7#'
+SECRET_KEY = '@*^caafkf*0tz#@$94xd1u3!51ip=k-628$ne-h3yx9!+@ikyx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- I4G005000HWO/blog/views.py
+++ I4G005000HWO/blog/views.py
@@ -1,3 +1,37 @@
+from dataclasses import field
+from re import L
+from django.forms import models
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from blog.models import Post
 
 # Create your views here.
+
+class PostListView(ListView):
+    models = Post
+
+
+class  PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+
+class  PostDetailView(DetailView):
+    models = Post
+
+
+class PostUpdateView(UpdateView):
+    models = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+
+class PostDeleteView(DeleteView):
+    models = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
